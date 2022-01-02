e56260e6669b67183a6694a5894503a66dc69a89
Gvidas <tarkimgvidas@gmail.com>
2022-01-02 15:49:44 +0200

    link added

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -5,11 +5,29 @@ import { NavLink } from "react-router-dom";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>Users Web App</div>
+      <div className={classes.logo}>
+        Users Web App
+        <a
+        className={classes.linkToGit}
+          href="https://github.com/GvidasGVD/usersWebApp/tree/master/src"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See code in GitHub
+        </a>
+      </div>
       <nav className={classes.nav}>
         <ul>
-          <li><NavLink to='/users' activeClassName={classes.active}>All Users</NavLink></li>
-          <li><NavLink to='/new-user' activeClassName={classes.active}>Add New User</NavLink></li>
+          <li>
+            <NavLink to="/users" activeClassName={classes.active}>
+              All Users
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new-user" activeClassName={classes.active}>
+              Add New User
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
